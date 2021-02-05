8f563f41d863d056a593511ec4fc6d3ddf2d146c
adithya <chreddy97@gmail.com>
2021-02-04 19:14:22 -0800

    improved ui and added users list

---
+++ app/models/course.rb
@@ -0,0 +1,15 @@
+class Course < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true, length: { :minimum => 5 }
+
+  belongs_to :user
+
+  def to_s
+    title
+  end  
+  
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  has_rich_text :description
+end
